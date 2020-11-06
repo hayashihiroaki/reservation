d619cf09367306c83af4193759e846a538dd8d2a
hayashihiroaki <61njn0@gmail.com>
2020-11-06 12:03:06 +0900

    レイアウト修正

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,6 +1,6 @@
 export const products = [
   {
-    coverImage: './assets/img/phone-cover.jpg',
+    coverImage: './assets/img/photo-1543069190-9d380c458bc2.jpg',
     name: 'Phone XL',
     price: 799,
     description: 'A large phone with one of the best screens',
@@ -12,7 +12,7 @@ export const products = [
     text3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
   },
   {
-    coverImage: './assets/img/phone-cover.jpg',
+    coverImage: './assets/img/photo-1584438784894-089d6a62b8fa.jpg',
     name: 'Phone Mini',
     price: 699,
     description: 'A great phone with one of the best cameras',
@@ -24,7 +24,7 @@ export const products = [
     text3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
   },
   {
-    coverImage: './assets/img/phone-cover.jpg',
+    coverImage: './assets/img/photo-1509395062183-67c5ad6faff9.jpg',
     name: 'Phone Standard',
     price: 299,
     description: '',
@@ -36,7 +36,7 @@ export const products = [
     text3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
   },
   {
-    coverImage: './assets/img/phone-cover.jpg',
+    coverImage: './assets/img/photo-1565849904461-04a58ad377e0.jpg',
     name: 'Phone Special',
     price: 999,
     description: '',
